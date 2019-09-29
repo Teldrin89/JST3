908dd5408fcaea9e2b037fcf6e8f781eed46a47c
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-09-29 14:55:42 +0200

    created the draw pie function

--- jstut3.js
+++ jstut3.js
@@ -71,8 +71,29 @@ function createRandomColorArray(){
     for(i = 0; i < expendituresArray.length; i++){
         // populate the array of random colors using the 16-based number system for color definition (for example #FFFFFF is a code for black color) that has a max value of 16777215 in decimal - hence use the random function (that generates value between 0 and 1) and multiply it by max value, round it up to integer and change to 16-based system adding "#" before
         randColArr[i] = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        // floor - for rounding up to integer, random - for generating random number between 1 and 1, toString - to change the result of math operation to string and with parameter "16" it will change to 16-based value (color)
+        // floor - for rounding up to integer, random - for generating random number between 1 and 0, toString - to change the result of math operation to string and with parameter "16" it will change to 16-based value (color)
     }
     return randColArr;
     // TODO: consider adding pallete of colors (like ENDESGA32) and looping/randommizing the colors from that pallet as colors for pie pieces of the chart
+}
+
+// function to draw a single pie part of the chart
+function drawPie(){
+    // get a canvas refrence
+    var canvas = document.getElementById('canvas');
+    // create a "context" for a 2D chart - like "x" and "y" coordinates
+    var context = canvas.getContext('2d');
+    // set a starting and ending angle for single pie piece
+    var startAngle = 0;
+    var endAngle = 0;
+    // loop for drawing as many slices as there is different percentages
+    for(i = 0; i < percentArray.length; i++){
+        // for start reset the start and end angle to be identical - in case it's the next slice the new start well be the old end
+        startAngle = endAngle;
+        // new end angle is calculated using the percentage of the precent array that is already adjusted to take the correct piece of circular pie chart
+        endAngle = endAngle + (percentArray[i] * Math.PI);
+        // separate function called to draw single slice
+        drawSlice(context, 300, 200, 150, startAngle, endAngle, colorArray[i])
+
+    }
 }
